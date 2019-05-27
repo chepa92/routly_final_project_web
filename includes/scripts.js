ca06a7ffdaaa59185bb0b3c2ae84cff0e4c9bc2a
Yura R <chepa92@gmail.com>
2019-05-27 22:14:38 +0300

    all table filters works (except free search)

--- includes/scripts.js
+++ includes/scripts.js
@@ -18,18 +18,22 @@ $(document).ready(function () {
                 '<td>' + '<input type="submit" name="stationID" class="edit_button" value=' + row.StationId + "></input></td>" +
                 '</tr>'
             )
-            var option = $(
+            var optionDist = $(
                 '<option>' + row.District + '</option>'
             )
-            $('#District').append(option)
+            $('#District').append(optionDist)
+            $('tbody').append(table_row)
+
+
+            var optionCity = $(
+                '<option>' + row.City + '</option>'
+            )
+            $('#City').append(optionCity)
             $('tbody').append(table_row)
         }
 
-        $("select option").val(function(idx, val) {
-            $(this).siblings('[value="' + val + '"]').remove();
-        })
-
     });
+
 
 
     $('#District').on('change', function (e) {
@@ -52,5 +56,54 @@ $(document).ready(function () {
         }
         $('tbody').html(match)
     })
+
+    $('#City').on('change', function (e) {
+        var value = e.target.value
+        var match = ''
+        console.log(value)
+        for (var row of json_data) {
+            if (row.City == value || value == "all")
+                match +=
+                    '<tr>' +
+                    '<td scope="row">' + row.StationId + '</td>' +
+                    '<td>' + row.StationName + '</td>' +
+                    '<td>' + row.District + '</td>' +
+                    '<td>' + row.City + '</td>' +
+                    '<td>' + row.Street + '</td>' +
+                    '<td>' + row.Smart.Wifi + '</td>' +
+                    '<td>' + row.Comment + '</td>' +
+                    '<td>' + '<input type="submit" name="stationID" class="edit_button" value=' + row.StationId + "></input></td>" +
+                    '</tr>'
+        }
+        $('tbody').html(match)
+    })
+
+    $('#Smart').on('change', function (e) {
+        var value = e.target.value
+        var match = ''
+        console.log(value)
+        for (var row of json_data) {
+            if (row.Smart.Wifi == value || value == "all")
+                match +=
+                    '<tr>' +
+                    '<td scope="row">' + row.StationId + '</td>' +
+                    '<td>' + row.StationName + '</td>' +
+                    '<td>' + row.District + '</td>' +
+                    '<td>' + row.City + '</td>' +
+                    '<td>' + row.Street + '</td>' +
+                    '<td>' + row.Smart.Wifi + '</td>' +
+                    '<td>' + row.Comment + '</td>' +
+                    '<td>' + '<input type="submit" name="stationID" class="edit_button" value=' + row.StationId + "></input></td>" +
+                    '</tr>'
+        }
+        $('tbody').html(match)
+    })
+
+    var optionBool = $(
+        '<option>' + 'True' + '</option>' +
+        '<option>' + 'False' + '</option>'
+    )
+    $('#Smart').append(optionBool)
+    $('tbody').append(table_row)
 
 });
