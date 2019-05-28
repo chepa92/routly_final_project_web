a91e437a5a1dbd83e1968eae400d970364010ce0
Rom Fatal <39658012+RomFatal@users.noreply.github.com>
2019-05-28 03:24:20 +0300

    bug Fix choose District

--- includes/scripts.js
+++ includes/scripts.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
 
     var json_data = []
     $.getJSON("data/MOCK_DATA.json", function (data) {
+        var flag=0;
         json_data = data
         for (var row of data) {
             var table_row = $(
@@ -18,18 +19,42 @@ $(document).ready(function () {
                 '<td>' + '<input type="submit" name="stationID" class="edit_button" value=' + row.StationId + "></input></td>" +
                 '</tr>'
             )
+            
+                children = District.childNodes
+                for (var i = 3; i < children.length; i++) {
+                   var option= children[i];
+                   if(district==row.District)
+                               flag=1;
+                }
+            if(flag==0)
+            {
             var optionDist = $(
                 '<option>' + row.District + '</option>'
             )
             $('#District').append(optionDist)
             $('tbody').append(table_row)
+            }
+            flag=0;
 
 
+
+            children = City.childNodes
+                for (var i = 3; i < children.length; i++) {
+                   var option= children[i]
+                   var city = option.textContent;
+                   if(city==row.City)
+                               flag=1;
+                }
+                
+            if(flag==0)
+            {
             var optionCity = $(
                 '<option>' + row.City + '</option>'
             )
             $('#City').append(optionCity)
             $('tbody').append(table_row)
+            }
+            flag=0;
         }
 
     });
