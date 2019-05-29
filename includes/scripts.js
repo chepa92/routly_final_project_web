8a50b7c2af3ed350fb96441780b5fda4d25a8f56
CHEPA <chepa@bigeek.net>
2019-05-29 20:29:22 +0300

    minimize js

--- includes/scripts.js
+++ includes/scripts.js
@@ -1,21 +1,20 @@
 $(document).ready(function () {
-
-
+    
     // getting stationID to parameter
-    function findGetParameter() {
+    function findGetParameter(parameterName) {
         var result = null,
             tmp = [];
         location.search
             .substr(1)
             .split("&")
             .forEach(function (item) {
-              tmp = item.split("=");
-              if (tmp[0] === 'stationID') result = decodeURIComponent(tmp[1]);
+                tmp = item.split("=");
+                if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
             });
         return result;
     }
 
-    let stationID = findGetParameter();
+    let stationID = findGetParameter('stationID');
 
     var json_data = []
     $.getJSON("data/MOCK_DATA.json", function (data) {
@@ -23,7 +22,7 @@ $(document).ready(function () {
 
         var i;
         for (i = 0; i < json_data.length; ++i) {
-            if (json_data[i].StationId == stationID){
+            if (json_data[i].StationId == stationID) {
                 $('#StationID').attr('value', json_data[i].StationId)
                 $('#StationName').attr('value', json_data[i].StationName)
                 $('#District').attr('value', json_data[i].District)
@@ -32,25 +31,23 @@ $(document).ready(function () {
                 $('#Latitude').attr('value', json_data[i].Latitude)
                 $('#Longitude').attr('value', json_data[i].Longitude)
                 $('#Comment').attr('value', json_data[i].Comment)
-                
+
                 if (json_data[i].Smart.SmartScreen == 'True')
-                $('input[name=Smart1').prop('checked',true)
+                    $('input[name=Smart1').prop('checked', true)
                 if (json_data[i].Smart.Conditioner == 'True')
-                $('input[name=Smart2').prop('checked',true)
+                    $('input[name=Smart2').prop('checked', true)
                 if (json_data[i].Smart.Light == 'True')
-                $('input[name=Smart3').prop('checked',true)
+                    $('input[name=Smart3').prop('checked', true)
                 if (json_data[i].Smart.Wifi == 'True')
-                $('input[name=Smart4').prop('checked',true)
+                    $('input[name=Smart4').prop('checked', true)
                 break;
             }
         }
     });
 
-
-
     var json_data = []
     $.getJSON("data/MOCK_DATA.json", function (data) {
-        var flag=0;
+        var flag = 0;
         json_data = data
         for (var row of data) {
             var table_row = $(
@@ -62,109 +59,79 @@ $(document).ready(function () {
                 '<td>' + row.Street + '</td>' +
                 '<td>' + row.Smart.Wifi + '</td>' +
                 '<td>' + row.Comment + '</td>' +
-                '<td>' + '<input type="submit" name="stationID" class="edit_button" value=' + row.StationId + "></input></td>" +
+                '<td>' + '<input type="submit" name="stationID" class="btn btn-success btn-circle" value=' + row.StationId + "></input></td>" +
                 '</tr>'
-                // '<input type="image" name="stationID" src="/images/information.png" value=' + row.StationId + "></input></td>" +
             )
-        
+
             children = District.childNodes;
             for (var i = 3; i < children.length; i++) {
-                var option= children[i]
+                var option = children[i]
                 var district = option.textContent;
-                if(district==row.District)
-                    flag=1;
+                if (district == row.District)
+                    flag = 1;
             }
 
-        if(flag==0){
-        var optionDist = $('<option>' + row.District + '</option>')
-        }
+            if (flag == 0) {
+                var optionDist = $('<option>' + row.District + '</option>')
+            }
 
-        $('#District').append(optionDist)
-        $('tbody').append(table_row)
-        flag=0;
+            $('#District').append(optionDist)
+            $('tbody').append(table_row)
+            flag = 0;
 
-        children = City.childNodes
+            children = City.childNodes
             for (var i = 3; i < children.length; i++) {
-                var option= children[i]
+                var option = children[i]
                 var city = option.textContent;
-                if(city==row.City)
-                flag=1;
+                if (city == row.City)
+                    flag = 1;
             }
-            
-        if(flag==0){
-            var optionCity = $(
-            '<option>' + row.City + '</option>')
+
+            if (flag == 0) {
+                var optionCity = $(
+                    '<option>' + row.City + '</option>')
+            }
+            $('#City').append(optionCity)
+            $('tbody').append(table_row)
+            flag = 0;
         }
-        $('#City').append(optionCity)
-        $('tbody').append(table_row)
-        flag=0;
-    }
 
     });
 
-
-
-    $('#District').on('change', function (e) {
-        var value = e.target.value
-        var match = ''
-        console.log(value)
+    function UpdateTable(value, match, parameter = '', parameter2 = '') {
         for (var row of json_data) {
-            if (row.District == value || value == "all")
+            let tmp
+            if (parameter2 == '') {
+                tmp = row[parameter]
+            } else {
+                tmp = row[parameter][parameter2]
+            }
+            if (tmp == value || value == "all")
                 match +=
-                    '<tr>' +
-                    '<td scope="row">' + row.StationId + '</td>' +
-                    '<td>' + row.StationName + '</td>' +
-                    '<td>' + row.District + '</td>' +
-                    '<td>' + row.City + '</td>' +
-                    '<td>' + row.Street + '</td>' +
-                    '<td>' + row.Smart.Wifi + '</td>' +
-                    '<td>' + row.Comment + '</td>' +
-                    '<td>' + '<input type="submit" name="stationID" class="edit_button" value=' + row.StationId + "></input></td>" +
-                    '</tr>'
+                '<tr>' +
+                '<td scope="row">' + row.StationId + '</td>' +
+                '<td>' + row.StationName + '</td>' +
+                '<td>' + row.District + '</td>' +
+                '<td>' + row.City + '</td>' +
+                '<td>' + row.Street + '</td>' +
+                '<td>' + row.Smart.Wifi + '</td>' +
+                '<td>' + row.Comment + '</td>' +
+                '<td>' + '<input type="submit" name="stationID" class="btn btn-success btn-circle" value=' + row.StationId + "></input></td>" +
+                '</tr>'
         }
         $('tbody').html(match)
+    }
+
+    $('#District').on('change', function (e) {
+        UpdateTable(e.target.value, '', 'District');
     })
 
     $('#City').on('change', function (e) {
-        var value = e.target.value
-        var match = ''
-        console.log(value)
-        for (var row of json_data) {
-            if (row.City == value || value == "all")
-                match +=
-                    '<tr>' +
-                    '<td scope="row">' + row.StationId + '</td>' +
-                    '<td>' + row.StationName + '</td>' +
-                    '<td>' + row.District + '</td>' +
-                    '<td>' + row.City + '</td>' +
-                    '<td>' + row.Street + '</td>' +
-                    '<td>' + row.Smart.Wifi + '</td>' +
-                    '<td>' + row.Comment + '</td>' +
-                    '<td>' + '<input type="submit" name="stationID" class="edit_button" value=' + row.StationId + "></input></td>" +
-                    '</tr>'
-        }
-        $('tbody').html(match)
+        UpdateTable(e.target.value, '', 'City');
     })
 
     $('#Smart').on('change', function (e) {
-        var value = e.target.value
-        var match = ''
-        console.log(value)
-        for (var row of json_data) {
-            if (row.Smart.Wifi == value || value == "all")
-                match +=
-                    '<tr>' +
-                    '<td scope="row">' + row.StationId + '</td>' +
-                    '<td>' + row.StationName + '</td>' +
-                    '<td>' + row.District + '</td>' +
-                    '<td>' + row.City + '</td>' +
-                    '<td>' + row.Street + '</td>' +
-                    '<td>' + row.Smart.Wifi + '</td>' +
-                    '<td>' + row.Comment + '</td>' +
-                    '<td>' + '<input type="submit" name="stationID" class="edit_button" value=' + row.StationId + "></input></td>" +
-                    '</tr>'
-        }
-        $('tbody').html(match)
+        UpdateTable(e.target.value, '', 'Smart', 'Wifi');
     })
 
     var optionBool = $(
